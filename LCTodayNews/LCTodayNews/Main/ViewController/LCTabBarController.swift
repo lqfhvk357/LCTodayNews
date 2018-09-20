d0ba21140dbd281d32cf43f2c8a5c46417b65f87
lc <58054863@qq.com>
2018-09-20 12:43:31 +0800

    add tabar icon

---
+++ LCTodayNews/LCTodayNews/Main/ViewController/LCTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  LCTabBarController.swift
+//  LCTodayNews
+//
+//  Created by 罗超 on 2018/9/20.
+//  Copyright © 2018年 罗超. All rights reserved.
+//
+
+import UIKit
+
+class LCTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     //  let item = UITabBarItem.init(title: "", image: <#T##UIImage?#>, selectedImage: <#T##UIImage?#>)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
