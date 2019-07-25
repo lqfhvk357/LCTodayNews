cf56c6cddc474d3e49b09b2948f0135c25a2200f
lc <58054863@qq.com>
2019-07-26 07:28:44 +0800

    try?

--- LCTodayNews/LCTodayNewsTests/LCTodayNewsTests.swift
+++ LCTodayNews/LCTodayNewsTests/LCTodayNewsTests.swift
@@ -15,11 +15,6 @@ class LCTodayNewsTests: XCTestCase {
     }
 
     override func tearDown() {
-        let view = UIView()
-        view.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
-        view.backgroundColor = .red
-        view.alpha = 0.5
-        view.layer.bounds = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
